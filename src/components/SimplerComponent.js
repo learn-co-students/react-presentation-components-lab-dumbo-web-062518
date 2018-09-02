73056e12213d08688832ae7cd3e0920cac431c17
runandrerun <the.asantiagojr@gmail.com>
2018-09-02 13:07:58 -0400

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,17 @@
 // Code SimplerComponent Here
+import React, { Component } from 'react';
+
+// export default class SimplerComponent extends Component {
+//   render() {
+//     return (
+//       <div onClick={this.undefined}>I am just happy</div>
+//     )
+//   }
+// }
+
+const SimplerComponent = ({handleClick}) => {
+  return (
+    <div onClick={handleClick}>I am just happy</div>
+  )
+}
+export default SimplerComponent;
